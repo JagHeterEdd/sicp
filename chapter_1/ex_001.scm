0ca6ee5c6ed3c0dbe7ffe8cce2cbfe7a65e39ad6
JagHeterEdd <edd@schauman-haigh.se>
2023-01-23 12:25:54 -0800

    Finish ch1-ex1

--- chapter_1/ex_001.scm
+++ chapter_1/ex_001.scm
@@ -2,8 +2,46 @@
 
 (export ans_001)
 (def (ans_001 . args)
-  (displayln 10)         ;; 10
-  (displayln (+ 5 3 4))  ;; 12
-  (displayln (- 9 1)))   ;;  8
+    (displayln 10)
+
+    (displayln 
+        (+ 5 3 4))
+
+    (displayln 
+        (- 9 1))
+
+    (displayln 
+        (/ 6 2))
+
+    (displayln 
+        (+ (* 2 4) (- 4 6)))
+
+    (define a 3)
+    (define b (+ a 1))
+
+    (displayln 
+        (+ a b (* a b)))
+
+    (displayln 
+        (= a b))
+
+    (displayln 
+        (if (and (> b a) (< b (* a b)))
+            b 
+            a))
+
+    (displayln 
+        (cond ((= a 4) 6)
+              ((= b 4) (+ 6 7 a))
+              (else 25)))
+
+    (displayln 
+        (+ 2 (if (> b a) b a)))
+
+    (displayln 
+        (* (cond ((> a b) a) 
+                 ((< a b) b)
+                 (else -1)) 
+           (+ a 1))))
 
 (ans_001)
